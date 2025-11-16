340d99f2e752528951f2e9aa113800833e889636
EvgenMolev <evgenmolev1@gmail.com>
2025-11-16 23:03:10 +0300

    first commit

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home-page">
+    <NetworkCalculator />
+  </div>
+</template>
+
+<script setup lang="ts">
+import NetworkCalculator from '../components/NetworkCalculator.vue';
+</script>
+
+<style scoped>
+.home-page {
+  min-height: 100vh;
+  padding: 20px;
+  background-color: var(--color-background);
+}
+</style>
